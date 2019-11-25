1263d162e30312fbd7e236fb2a7007962645b0fa
ozlemakbas90@gmail.com <ozlemakbas90@gmail.com>
2019-11-25 23:40:59 +0300

    feat: add answer and add question features
    add answer
    add question features
    add loading

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,62 @@
+import { saveQuestion, saveQuestionAnswer } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_ANSWER = 'ADD_ANSWER'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function receiveQuestions (questions) {
   return {
     type: RECEIVE_QUESTIONS,
     questions,
+  }
+}
+
+function addQuestion (question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  }
+}
+
+function addAnswer (answer) {
+  return {
+    type: ADD_ANSWER,
+    answer,
+  }
+}
+
+export function handleAddQuestion (optionOneText, optionTwoText) {
+  return (dispatch, getState) => {
+    const { author } = getState()
+    dispatch(showLoading())
+
+    return saveQuestion({
+      optionOneText,
+      optionTwoText,
+      author,
+    })
+      .then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()))
+      .catch((e) => {
+        console.log('Add Answer action failed.')
+      })
+  }
+}
+
+export function handleAddAnswer (qId, answer, author) {
+  return (dispatch) => {
+    dispatch(showLoading())
+
+    return saveQuestionAnswer({
+      author,
+      qId,
+      answer,
+    })
+      .then((answer) => dispatch(addAnswer(answer)))
+      .then(() => dispatch(hideLoading()))
+      .catch((e) => {
+        console.log('Add Answer action failed.')
+      })
   }
 }
--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,11 +2,14 @@ import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
 import { setAuthedUser } from '../actions/authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 
 const AUTHED_ID = ''
 
 export function handleInitialData () {
   return (dispatch) => {
+    dispatch(showLoading())
     return getInitialData()
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users))
